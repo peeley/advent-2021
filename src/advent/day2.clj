53ee8a109e27d019bf8c4dbedef411ef2bf46480
Noah Snelson <noah.snelson@protonmail.com>
2021-12-24 14:41:05 -0800

    Day 2 part 2 done

--- src/advent/day2.clj
+++ src/advent/day2.clj
@@ -22,8 +22,22 @@
   (let [{:keys [x y]} (reduce reduce-coords {:x 0 :y 0} input)]
     (* x y)))
 
+(defn reduce-coords-with-aim
+  [{:keys [x y aim]} [command n]]
+  (case command
+    "forward" {:x (+ x n) :y (+ y (* aim n)) :aim aim}
+    "down" {:x x :y y :aim (+ aim n)}
+    "up" {:x x :y y :aim (- aim n)}))
+
+(defn part2
+  [input]
+  (let [{:keys [x y]} (reduce reduce-coords-with-aim {:x 0 :y 0 :aim 0} input)]
+    (* x y)))
+
 (comment
 
   (part1 input);; => 1804520
+
+  (part2 input);; => 1971095320
 
   ,)
