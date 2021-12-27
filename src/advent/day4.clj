6657f580a1dacc971328da866af72d172b73f00a
Noah Snelson <noah.snelson@protonmail.com>
2021-12-27 15:31:28 -0800

    Day 4 part 2 done

--- src/advent/day4.clj
+++ src/advent/day4.clj
@@ -54,7 +54,7 @@
   [(get-down-diag-set board)
    (get-up-diag-set board)])
 
-(defn get-possible-bingo-sets
+(defn get-possible-bingos
   [board]
   (->> [(get-col-sets board)
         (get-row-sets board)
@@ -83,7 +83,7 @@
 (defn part1
   [boards numbers]
   (loop [numbers numbers
-         bingo-sets (map get-possible-bingo-sets boards)]
+         bingo-sets (map get-possible-bingos boards)]
     (let [number-to-mark (first numbers)
           marked-cards (map #(mark-number-on-board % number-to-mark) bingo-sets)
           bingos (sets-with-bingo marked-cards)]
@@ -91,8 +91,26 @@
         (* number-to-mark (sum-unmarked-numbers (first bingos)))
         (recur (rest numbers) marked-cards)))))
 
+(defn part2
+  [boards numbers]
+  (loop [numbers numbers
+         bingo-sets (map get-possible-bingos boards)
+         last-winner nil
+         last-winning-number nil]
+    (let [number-to-mark (first numbers)
+          marked-cards (map #(mark-number-on-board % number-to-mark) bingo-sets)
+          new-bingo (first (sets-with-bingo marked-cards))]
+      (if (empty? (rest numbers))
+        (* last-winning-number (sum-unmarked-numbers last-winner))
+        (recur (rest numbers)
+               (remove board-has-bingo? marked-cards)
+               (if new-bingo new-bingo last-winner)
+               (if new-bingo number-to-mark last-winning-number))))))
+
 (comment
 
-  (part1 boards board-numbers)
+  (part1 boards board-numbers);; => 44088
+
+  (part2 boards board-numbers);; => 23670
 
   ,)
