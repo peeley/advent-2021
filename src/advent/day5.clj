bc2476bc65f00150f136fa39cd26d67d262643b6
Noah Snelson <noah.snelson@protonmail.com>
2021-12-28 01:22:54 -0800

    Day 5 part 2 done

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -30,9 +30,9 @@
   [[[x1 y1] [x2 y2]]]
   (let [[lowx highx] (sort [x1 x2])
         [lowy highy] (sort [y1 y2])]
-    (set (for [x (range lowx (inc highx))
-               y (range lowy (inc highy))]
-           [x y]))))
+    (for [x (range lowx (inc highx))
+          y (range lowy (inc highy))]
+      [x y])))
 
 (defn point-on-line?
   [{:keys [xs ys]} [x y]]
@@ -43,17 +43,43 @@
   [lines-points point]
   (count (filter #(point-on-line? % point) lines-points)))
 
+(defn num-points-with-two-lines
+  [line-points]
+  (->> line-points
+       (reduce #(update %1 %2 (fnil inc 0)) {})
+       (filter (fn [[_ v]] (> v 1)))
+       count))
+
 (defn part1
   [coord-pairs]
   (let [straight-lines (filter line-is-straight? coord-pairs)
         straight-line-points (mapcat get-points-on-line straight-lines)]
-    (->> straight-line-points
-         (reduce #(update %1 %2 (fnil inc 0)) {})
-         (filter (fn [[k v]] (> v 1)))
-         count)))
+    (num-points-with-two-lines straight-line-points)))
+
+(defn line-is-diagonal?
+  [[[x1 y1] [x2 y2]]]
+  (= (Math/abs (- x1 x2))
+     (Math/abs (- y1 y2))))
+
+(defn get-points-on-diagonal
+  [[[x1 y1] [x2 y2]]]
+  (let [xs (if (> x1 x2) (range x1 (dec x2) -1) (range x1 (inc x2)))
+        ys (if (> y1 y2) (range y1 (dec y2) -1) (range y1 (inc y2)))]
+    (map vector xs ys)))
+
+(defn part2
+  [coord-pairs]
+  (let [straight-lines (filter line-is-straight? coord-pairs)
+        straight-line-points (mapcat get-points-on-line straight-lines)
+        diagonal-lines (filter line-is-diagonal? coord-pairs)
+        diagonal-line-points (mapcat get-points-on-diagonal diagonal-lines)
+        all-line-points (concat straight-line-points diagonal-line-points)]
+    (num-points-with-two-lines all-line-points)))
 
 (comment
 
   (part1 input);; => 5576
+
+  (part2 input);; => 18144
 
   ,)
