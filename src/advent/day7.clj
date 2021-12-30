a9ae4c5428dcd6964737b47bc2a5e1a3c6a1eacb
Noah Snelson <noah.snelson@protonmail.com>
2021-12-30 12:34:05 -0800

    Day 7 part 2 done

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -8,30 +8,43 @@
        (str/split $ #",")
        (map #(Integer/parseInt %) $)))
 
+(defn linear-fuel-cost
+  [current-x destination-x]
+  (Math/abs (- current-x destination-x)))
+
 (defn total-fuel-to-move-to-x
-  [fleet-xs x]
+  [fuel-cost-fn fleet-xs x]
   (->> fleet-xs
-       (map #(Math/abs (- x %)))
+       (map #(fuel-cost-fn % x))
        (apply +)))
 
-(defn check-if-x-is-less-fuel
-  [current-min-fuel x fleet-xs]
-  (let [needed-fuel (total-fuel-to-move-to-x fleet-xs x)]
-    (if (< needed-fuel current-min-fuel)
-      needed-fuel
-      current-min-fuel)))
+(defn least-fuel-needed-to-align
+  [xs fuel-cost-fn]
+  (let [max-x (apply max xs)
+        min-x (apply min xs)
+        possible-xs (range min-x (inc max-x))]
+    (->> possible-xs
+         (map #(total-fuel-to-move-to-x fuel-cost-fn input %))
+         (apply min))))
 
 (defn part1
   [input]
-  (let [max-x (apply max input)
-        min-x (apply min input)
-        possible-xs (range min-x (inc max-x))]
-    (->> possible-xs
-         (map #(total-fuel-to-move-to-x input %))
-         (apply min))))
+  (least-fuel-needed-to-align input linear-fuel-cost))
+
+(defn exponential-fuel-cost
+  [current-x destination-x]
+  (let [x-diff (Math/abs (- current-x destination-x))]
+    (/ (* x-diff (+ x-diff 1))
+       2)))
+
+(defn part2
+  [input]
+  (least-fuel-needed-to-align input exponential-fuel-cost))
 
 (comment
 
   (part1 input);; => 336701
+
+  (part2 input);; => 95167302
 
   ,)
